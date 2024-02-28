55507d4207fbf9f49b7bad973d2b2eebb49abfdc
root <baffourcastro@gmail.com>
2024-02-28 01:00:05 -0800

    0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
-# creates a file in /tmp
-
+#The code will create a file name school inside the /tmp Directory
 file { '/tmp/school':
-	mode	=> '0744',
-	owner	=> 'www-data',
-	group	=> 'www-data',
+	ensure => file,
 	content => 'I love Puppet',
+	mode => '07744',
+	owner => 'www-data',
+	group => 'www-data',
 }
